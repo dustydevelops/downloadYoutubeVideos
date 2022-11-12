ed33e7f10a862f943cdd8cdb2c61af8a57aa2983
Dustin Michael Smith <Dustinmichaelsmith03@gmail.com>
2022-11-11 22:04:34 -0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,60 @@
+
+from tkinter import *
+import pytube
+
+
+
+def downloadVideo():
+    video_url = url.get()
+    try:
+        youtube = pytube.YouTube(video_url)
+        video = youtube.streams.get_highest_resolution()
+
+        video.download('videos')
+        notif.config(fg="green", text="Video Download Started")
+    except Exception as e:
+        print(e)
+        notif.config(fg="red", text="Video could not be downloaded")
+        
+        
+
+def downloadAudio():
+    video_url = url.get()
+    try:
+        youtube = pytube.YouTube(video_url)
+        audio = youtube.streams.get_audio_only()
+        audio.download('songs')
+        notif.config(fg='green',text= 'audio download started...)
+    except Exception as e:
+        
+        notif.config(fg="red", text="Audio could not  downloaded: ", e)
+
+def downloadBoth():
+  downloadAudio()
+  downloadVideo()
+
+
+
+# Main Screen
+master = Tk()
+master.title("Youtube Video Downloader")
+
+# Labels
+Label(master, text="Youtube Video Converter", fg="red", font=("Calibri", 15)).grid(sticky=N, padx=100, row=0)
+Label(master, text="Please enter the link to your video below : ", font=("Calibri", 15)).grid(sticky=N, row=1, pady=15)
+notif = Label(master, font=("Calibri", 12))
+notif.grid(sticky=N, pady=1, row=4)
+
+# Vars
+url = StringVar()
+# Entry
+Entry(master, width=50, textvariable=url).grid(sticky=N, row=2)
+
+# Buttons
+
+Button(master, width=20, text="Download Audio", font=("Calibri", 12), command=downloadAudio).grid(sticky=N, row=3, pady=0)
+Button(master, width=20, text="Download Video", font=("Calibri", 12), command=downloadVideo).grid(sticky=N, row=3, pady=45)
+Button(master, width=20, text="Download audio & video", font=("Calibri", 12), command=downloadBoth).grid(sticky=N, row=3, pady=90)
+
+
+master.mainloop()
